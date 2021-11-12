a91afe6d2146b7159dfbbd53b1b7c6352fdff364
Angel <angel@Angels-Mac.local>
2021-11-12 12:38:27 +0100

    Working on iOS.

--- ios/Plugin/FullScreenPlugin.swift
+++ ios/Plugin/FullScreenPlugin.swift
@@ -7,7 +7,29 @@ import Capacitor
  */
 @objc(FullScreenPlugin)
 public class FullScreenPlugin: CAPPlugin {
-    private let implementation = FullScreen()
+    
+    private let insets:UIEdgeInsets = UIEdgeInsets.zero;
+    
+    private let implementation = FullScreen();
+    
+    static func refreshInsets(bridge: CAPBridgeProtocol) {
+        let insets: UIEdgeInsets = bridge.webView!.safeAreaInsets;
+        let top: Int = Int(round(insets.top));
+        let left: Int = Int(round(insets.left));
+        let bottom: Int = Int(round(insets.bottom));
+        let right: Int = Int(round(insets.right));
+        let data: String = "{ \"detail\": { \"top\": \(top), \"left\": \(left), \"bottom\": \(bottom), \"right\": \(right) } }";
+        CAPLog.print("viewSafeAreaInsetsDidChange(top: \(top), left: \(left), bottom: \(bottom), right: \(right))");
+        bridge.triggerJSEvent(eventName: "insetschange", target: "document", data: data);
+    }
+    
+    override public func load() {
+        CAPLog.print("Loading FullScreenPlugin...");
+        super.load();
+        // UIResponder.keyboardWillShowNotification
+        // class ViewController: UIViewController
+        FullScreenPlugin.refreshInsets(bridge: bridge!);
+    }
 
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
@@ -15,4 +37,15 @@ public class FullScreenPlugin: CAPPlugin {
             "value": implementation.echo(value)
         ])
     }
+    
+    
+}
+
+extension CAPBridgeViewController {
+    
+    override public func viewSafeAreaInsetsDidChange() {
+        super.viewSafeAreaInsetsDidChange();
+        FullScreenPlugin.refreshInsets(bridge: bridge!);
+    }
+    
 }
