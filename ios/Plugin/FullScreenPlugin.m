8cd73751119839e8065932c085fe362c8ff54265
Angel <angel@Angels-Mac.local>
2021-11-13 13:13:07 +0100

    Changes.

--- ios/Plugin/FullScreenPlugin.m
+++ ios/Plugin/FullScreenPlugin.m
@@ -4,5 +4,10 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(FullScreenPlugin, "FullScreen",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(toggle, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getInsets, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(isVisible, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setColor, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setStyle, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(toggleScroll, CAPPluginReturnPromise);
 )
